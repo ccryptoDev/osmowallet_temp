c9c0969c7381c0b85458cb4072a257aa9058368f
Success126 <golbensta@gmail.com>
2024-05-06 07:22:36 -0400

    missing part

--- src/modules/wallets/wallets.module.ts
+++ src/modules/wallets/wallets.module.ts
@@ -4,12 +4,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Wallet } from 'src/entities/wallet.entity';
 import { CoinsModule } from '../coins/coins.module';
 import { WalletsController } from './wallets.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       Wallet,
     ]),
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
     CoinsModule,
   ],
   providers: [WalletsService],
